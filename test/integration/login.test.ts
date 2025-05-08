b8707e66bbeb134f93c0ae3ccda324708d72bc46
Bruno Zamorano Alves Monteiro <bzam1204@gmail.com>
2025-05-08 11:47:08 -0300

    feat: login

---
+++ test/integration/login.test.ts
@@ -0,0 +1,47 @@
+import Login from "@/domain/entities/login";
+import User from "@/domain/entities/user";
+
+import FieldAdminRepositoryMemory from "@/infrastructure/repositories/field-admin-repository-memory";
+import PlayerRepositoryMemory from "@/infrastructure/repositories/player-repository-memory";
+import UserRepositoryMemory from "@/infrastructure/repositories/user-repository-memory";
+
+import userProps from "@test/shared/user-props";
+
+describe('Entrar', function () {
+
+  it('Deve entrar no sistema', async function () {
+    const fieldAdminRepository = new FieldAdminRepositoryMemory();
+    const playerRepository = new PlayerRepositoryMemory();
+    const userRepository = new UserRepositoryMemory([new User({...userProps, email : 'user@example.com', id : '1'})]);
+    const hashingServiceStub = {hash : () => Promise.resolve('123'), compare : () => Promise.resolve(true)};
+    const tokenServiceStub = {signAccessToken : () => '123123', signRefreshToken : () => '321321'};
+    const login = new Login(fieldAdminRepository, playerRepository, userRepository, hashingServiceStub, tokenServiceStub);
+    const credentials = {email : 'user@example.com', password : '123123'};
+    const {accessToken, refreshToken} = await login.execute(credentials);
+    expect(accessToken).toBe('123123');
+    expect(refreshToken).toBe('321321');
+  });
+
+  it('Não deve entrar no sistema com senha incorreta', async function () {
+    const fieldAdminRepository = new FieldAdminRepositoryMemory();
+    const playerRepository = new PlayerRepositoryMemory();
+    const userRepository = new UserRepositoryMemory([new User({...userProps, email : 'user@example.com', id : '1'})]);
+    const hashingServiceStub = {hash : () => Promise.resolve('123'), compare : () => Promise.resolve(false)};
+    const tokenServiceStub = {signAccessToken : () => '123123', signRefreshToken : () => '321321'};
+    const login = new Login(fieldAdminRepository, playerRepository, userRepository, hashingServiceStub, tokenServiceStub);
+    const credentials = {email : 'user@example.com', password : '123123'};
+    await expect(login.execute(credentials)).rejects.toThrow('INVALID_CREDENTIALS');
+  });
+  
+  it('Não deve entrar no sistema com email incorreto', async function () {
+    const fieldAdminRepository = new FieldAdminRepositoryMemory();
+    const playerRepository = new PlayerRepositoryMemory();
+    const userRepository = new UserRepositoryMemory([new User({...userProps, email : 'user@example.com', id : '1'})]);
+    const hashingServiceStub = {hash : () => Promise.resolve('123'), compare : () => Promise.resolve(false)};
+    const tokenServiceStub = {signAccessToken : () => '123123', signRefreshToken : () => '321321'};
+    const login = new Login(fieldAdminRepository, playerRepository, userRepository, hashingServiceStub, tokenServiceStub);
+    const credentials = {email : 'wrong_email', password : '123123'};
+    await expect(login.execute(credentials)).rejects.toThrow('INVALID_CREDENTIALS');
+  });
+
+});
